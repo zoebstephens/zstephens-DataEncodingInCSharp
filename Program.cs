96c1f5c9b3c63d7d956711c3b66318eec8e41557
zoebstephens <zoebstephens@gmail.com>
2022-01-13 11:55:49 -0800

    chore: initialize project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zstephens_DataEncodingInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
