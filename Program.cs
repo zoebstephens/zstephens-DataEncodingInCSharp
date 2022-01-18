3a9d3d95295d7f989b6a6e28112a7cdcc7f4e225
zoebstephens <zoebstephens@gmail.com>
2022-01-18 10:25:26 -0800

    Challenge Progess!

--- Program.cs
+++ Program.cs
@@ -6,37 +6,44 @@ namespace zstephens_DataEncodingInCSharp
     {
         static void Main(string[] args)
         {
-            char ch;
-            ch = 'A';
-            Console.WriteLine($"ch is storing the value: {ch}");
+            // char ch;
+            // ch = 'A';
+            // Console.WriteLine($"ch is storing the value: {ch}");
 
-            int ch_decimal;
-            ch_decimal = ch;
-            Console.WriteLine($"The decimal value of ch is: {ch_decimal}");
+            // int ch_decimal;
+            // ch_decimal = ch;
+            // Console.WriteLine($"The decimal value of ch is: {ch_decimal}");
 
-            int toConvert;
-            Console.Write("Enter an Integer: ");
-            toConvert = int.Parse(Console.ReadLine());
+            // int toConvert;
+            // Console.Write("Enter an Integer: ");
+            // toConvert = int.Parse(Console.ReadLine());
 
-            char asChar;
-            asChar = (char)toConvert;
-            Console.WriteLine($"The integer {toConvert} cast to a char is '{asChar}'");
+            // char asChar;
+            // asChar = (char)toConvert;
+            // Console.WriteLine($"The integer {toConvert} cast to a char is '{asChar}'");
 
-            char b;
-            b = (char)('A' + 1);
-            Console.WriteLine($"'A' + 1 = '{b}'");
+            // char b;
+            // b = (char)('A' + 1);
+            // Console.WriteLine($"'A' + 1 = '{b}'");
 
-            char z;
-            z = (char)('Z' + 3);
-            Console.WriteLine($"'Z' + 3 = '{z}'");
+            // char z;
+            // z = (char)('Z' + 3);
+            // Console.WriteLine($"'Z' + 3 = '{z}'");
 
-            char f; 
-            f = (char)('F' - 5);
-            Console.WriteLine($"'F' - 5 = '{f}' ");
+            // char f; 
+            // f = (char)('F' - 5);
+            // Console.WriteLine($"'F' - 5 = '{f}' ");
 
             // char Khoor#Zruog$; 
             // Khoor#Zruog$ = (char)('Khoor#Zruog$' - 3);
             // Console.WriteLine($"'Khoor#Zruog$' - 3 = '{Khoor#Zruog$}' ");
+
+            Cipher cipher;
+            cipher = new Cipher(5);
+
+            string encrypted;
+            encrypted = cipher.Encrypt("rosebud");
+            Console.WriteLine($"The encyrpted message is: '{encrypted}'");
 
 
 
