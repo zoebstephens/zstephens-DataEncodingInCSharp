30ff055dffb4b9b54a1e9b998c9869550720d244
zoebstephens <zoebstephens@gmail.com>
2022-01-18 01:12:17 -0800

    Work on Code

--- Program.cs
+++ Program.cs
@@ -9,6 +9,22 @@ namespace zstephens_DataEncodingInCSharp
             char ch;
             ch = 'A';
             Console.WriteLine($"ch is storing the value: {ch}");
+
+            int ch_decimal;
+            ch_decimal = ch;
+            Console.WriteLine($"The decimal value of ch is: {ch_decimal}");
+
+            int toConvert;
+            Console.Write("Enter an Integer: ");
+            toConvert = int.Parse(Console.ReadLine());
+
+            char asChar;
+            asChar = (char)toConvert;
+            Console.WriteLine($"The integer {toConvert} cast to a char is '{asChar}'");
+
+            char b;
+            b = (char)('A' + 1);
+            Console.WriteLine($"'A' + 1 = '{b}'");
         }
     }
 }
